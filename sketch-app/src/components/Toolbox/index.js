b8b534f84c3402e653702e5d7ad65e729c708645
KartikNaik0x <naikkartik86@gmail.com>
2024-01-15 09:49:56 +0530

    minor changes

--- sketch-app/src/components/Toolbox/index.js
+++ sketch-app/src/components/Toolbox/index.js
@@ -1,4 +1,5 @@
 import styles from './index.module.css'
+import {COLORS} from '@/constants'
 
 
 const Toolbox = () =>{
@@ -7,16 +8,16 @@ const Toolbox = () =>{
 
     }
     return(
-        <div style={styles.toolboxContainer}>
-            <div  style={styles.toolItem}>
-                <h4  style={styles.toolText}>Stroke Color</h4>
-                <div style={styles.itemContainer}>
-                    <div style={styles.colorBox}/>
+        <div className={styles.toolboxContainer}>
+            <div  className={styles.toolItem}>
+                <h4  className={styles.toolText}>Stroke Color</h4>
+                <div className={styles.itemContainer}>
+                    <div className={styles.colorBox} style={{backgroundColor:COLORS.BLACK}}/>
                 </div>
             </div>
-            <div  style={styles.toolItem}>
-                <h4 style={styles.toolText}>Brush Size</h4>
-                <div style={styles.itemContainer}>
+            <div  className={styles.toolItem}>
+                <h4 className={styles.toolText}>Brush Size</h4>
+                <div className={styles.itemContainer}>
                     <input type="range" min={1} max={10} step={1} onChange={updateBrushSize}/>
                 </div>
             </div>
